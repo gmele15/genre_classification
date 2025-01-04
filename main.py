8e479c6bde4133ae47633e55e92a17e2c1183cfd
Gumaro Melendez <cloudernew@gmail.com>
2025-01-03 20:55:35 -0700

    fix syntax error

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def go(config: DictConfig):
             "main",
             parameters={
                 "reference_artifact": config["data"]["reference_dataset"],
-                "sample_artifact": "preprocessed_data.csv:latest"
+                "sample_artifact": "preprocessed_data.csv:latest",
                 "ks_alpha": config["data"]["ks_alpha"]
             },
         )
